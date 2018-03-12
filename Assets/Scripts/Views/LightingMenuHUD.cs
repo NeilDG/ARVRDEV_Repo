33855354db00fff9ede63656bd1fafaf8f557529
NeilDG <NeilDG@github.com>
2018-03-12 11:28:10 +0800

    Polishes and setup for the visual effects topic

---
+++ Assets/Scripts/Views/LightingMenuHUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingMenuHUD : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnMenuClicked() {
+		LoadManager.Instance.LoadScene (SceneNames.VISUAL_EFFECTS_MENU_SCENE);
+	}
+}
