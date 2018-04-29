1a44e1a6f0c6f33ff8690209fcf1457420cc7413
NeilDG <NeilDG@github.com>
2018-04-29 17:11:34 +0800

    Created initial camera config to handle auto focus modes for differente devices.

---
+++ Assets/Scripts/Camera/ARCameraConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ARCameraConfig : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		VuforiaARController.Instance.RegisterVuforiaStartedCallback (this.OnVuforiaStarted);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+		}
+	}
+
+
+	private void OnVuforiaStarted() {
+		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+
+		if (CameraDevice.Instance.SetFrameFormat(Image.PIXEL_FORMAT.GRAYSCALE, true))
+		{
+			Debug.Log("Successfully registered pixel format " + Image.PIXEL_FORMAT.GRAYSCALE);
+
+		}
+		else
+		{
+			Debug.LogError(
+				"Failed to register pixel format " + Image.PIXEL_FORMAT.GRAYSCALE +
+				"\n the format may be unsupported by your device;" +
+				"\n consider using a different pixel format.");
+		}
+	}
+}
