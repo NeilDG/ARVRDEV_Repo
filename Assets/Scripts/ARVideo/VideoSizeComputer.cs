0a195577f426b894095725e26aa0caf1c48d5402
NeilDG <NeilDG@github.com>
2018-07-17 22:01:28 +0800

    Checkpoint for video adjustment using multiple anchors.

--- Assets/Scripts/ARVideo/VideoSizeComputer.cs
+++ Assets/Scripts/ARVideo/VideoSizeComputer.cs
@@ -17,6 +17,7 @@ public class VideoSizeComputer : MonoBehaviour {
 
     [SerializeField] private VideoTargetRef firstTarget;
     [SerializeField] private VideoTargetRef secondTarget;
+    [SerializeField] private Transform videoPlane;
 
     private bool firstTargetDetected = false;
     private bool secondTargetDetected = false;
@@ -25,8 +26,11 @@ public class VideoSizeComputer : MonoBehaviour {
     private float timeout = 1.0f;
 
     private float distanceApart = 0.0f; //the distance between the first vs second target
+    private const float SCALING_FACTOR = 0.2f;
 
     private VideoDebugScreen debugScreen;
+
+    private Vector3 baseVideoSize;
 
     private void Awake() {
         sharedInstance = this;
@@ -39,6 +43,9 @@ public class VideoSizeComputer : MonoBehaviour {
     // Use this for initialization
     void Start () {
         this.debugScreen = (VideoDebugScreen) ViewHandler.Instance.Show(ViewNames.VIDEO_DEBUG_SCREEN);
+
+        this.baseVideoSize = this.videoPlane.localScale;
+        this.videoPlane.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -52,8 +59,21 @@ public class VideoSizeComputer : MonoBehaviour {
 
             this.distanceApart = Vector2.Distance(this.firstTarget.transform.position, this.secondTarget.transform.position);
             this.debugScreen.DisplayDistance(this.distanceApart);
+
+            this.Resize();
         }
 	}
+
+    private void Resize() {
+        Vector3 newScale = Vector3.zero;
+        newScale.y = this.baseVideoSize.y;
+
+        newScale.x = this.baseVideoSize.x + (this.distanceApart * SCALING_FACTOR);
+        newScale.z = this.baseVideoSize.z + (this.distanceApart * SCALING_FACTOR);
+
+        this.videoPlane.localScale = newScale;
+        this.videoPlane.gameObject.SetActive(true);
+    }
 
     public void OnDetected(VideoTargetRef videoTargetRef) {
        if(videoTargetRef == this.firstTarget && !this.firstTargetDetected) {
