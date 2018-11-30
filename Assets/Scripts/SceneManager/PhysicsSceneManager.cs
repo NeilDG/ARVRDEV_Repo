46df2dd2e6a023c6622f3c6785e3cd9f96c12eb5
NeilDG <neil.delgallego@dlsu.edu.ph>
2018-11-30 20:27:53 +0800

    - Removed demo structures - Added dengue structure - Enabled dynamic position tracker.

---
+++ Assets/Scripts/SceneManager/PhysicsSceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// Stops the positional device tracker to make physics work effectively.
+/// </summary>
+public class PhysicsSceneManager : MonoBehaviour {
+
+    private static PhysicsSceneManager sharedInstance = null;
+    public PhysicsSceneManager Instance {
+        get {
+            return sharedInstance;
+        }
+    }
+
+    private void Awake() {
+        sharedInstance = this;
+
+    }
+    // Use this for initialization
+    void Start () {
+        VuforiaARController.Instance.RegisterVuforiaInitializedCallback(this.OnARStart);
+    }
+
+    private void OnDestroy() {
+        VuforiaARController.Instance.UnregisterVuforiaInitializedCallback(this.OnARStart);
+        sharedInstance = null; 
+    }
+
+    private void OnARStart() {
+       bool success = TrackerManager.Instance.DeinitTracker<PositionalDeviceTracker>();
+       Debug.Log("Successfully paused device tracker!");
+    }
+}
