cc7388e7d0f8fc6c2f278a085e08cf6d47afb25f
NeilDG <neil.delgallego@dlsu.edu.ph>
2018-12-24 07:26:17 +0800

    Added  AR wrecking ball scene template

---
+++ Assets/Scripts/ARWreckingBall/WreckingBallTargetHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class WreckingBallTargetHandler : ImageTargetBehaviour {
+
+    private GameObject playContainer;
+
+	// Use this for initialization
+	void Start () {
+        this.playContainer = this.transform.Find("Container").gameObject;
+        this.playContainer.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnTrackerUpdate(Status newStatus) {
+        base.OnTrackerUpdate(newStatus);
+        if (newStatus == Status.TRACKED) {
+            this.playContainer.SetActive(true);
+        }
+        else if(newStatus == Status.NO_POSE) {
+            this.playContainer.SetActive(false);
+        }
+    }
+
+
+}
