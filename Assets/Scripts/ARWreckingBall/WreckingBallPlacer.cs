783abb538b1f54dccbbaab105967b1424e90ae10
NeilDG <neil.delgallego@dlsu.edu.ph>
2018-12-26 15:10:00 +0800

    Update WreckingBallPlacer.cs

--- Assets/Scripts/ARWreckingBall/WreckingBallPlacer.cs
+++ Assets/Scripts/ARWreckingBall/WreckingBallPlacer.cs
@@ -12,6 +12,7 @@ public class WreckingBallPlacer : MonoBehaviour {
     [SerializeField] private GameObject activePlatform;
 
     private bool plotSucess = false;
+    private Vector3 baseOrigin;
     private const float Z_COORD = 15.0f;
     private const float Y_COORD = 4.0f;
 
@@ -19,6 +20,7 @@ public class WreckingBallPlacer : MonoBehaviour {
 	void Start () {
         EventBroadcaster.Instance.AddObserver(EventNames.ARWreckBallEvents.ON_RESET_CLICKED, this.OnResetSetupClicked);
 
+        this.baseOrigin = this.wbBase.transform.localPosition;
         this.wbJoints.SetActive(false);
 	}
 
@@ -34,6 +36,9 @@ public class WreckingBallPlacer : MonoBehaviour {
     public void MarkTargetLost() {
         this.plotSucess = false;
         this.wbJoints.SetActive(false);
+        this.wbBase.transform.SetParent(this.transform.parent);
+        this.wbBase.transform.localPosition = this.baseOrigin;
+        this.wbBase.transform.localRotation = Quaternion.identity;
     }
 
     public void PlotWreckingBall() {
@@ -41,7 +46,12 @@ public class WreckingBallPlacer : MonoBehaviour {
             return;
         }
 
-        Ray ray = this.arCamera.ScreenPointToRay(this.arCamera.WorldToScreenPoint(this.transform.position));
+        this.wbBase.SetActive(true);
+        this.wbBase.transform.SetParent(this.arCamera.transform);
+        this.wbJoints.SetActive(true);
+        this.plotSucess = true;
+
+        /*Ray ray = this.arCamera.ScreenPointToRay(this.arCamera.WorldToScreenPoint(this.transform.position));
         Debug.DrawRay(ray.origin, ray.direction, Color.red, 5.0f);
 
         RaycastHit hit;
@@ -51,14 +61,15 @@ public class WreckingBallPlacer : MonoBehaviour {
             Debug.Log("Hit pos: " + hitPos + " at object: " + hit.transform.gameObject.name);
 
             this.wbBase.SetActive(true);
-            Vector3 pos = this.wbBase.transform.localPosition;
+            /*Vector3 pos = this.wbBase.transform.localPosition;
             pos.x = hitPos.x;
             pos.y = Y_COORD;
             pos.z = Z_COORD;
             this.wbBase.transform.localPosition = pos;
+            this.wbBase.transform.SetParent(this.arCamera.transform);
             this.wbJoints.SetActive(true);
             this.plotSucess = true;
-        }
+        }*/
     }
 
 
